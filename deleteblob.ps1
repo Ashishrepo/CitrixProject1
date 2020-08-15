49605af50df32e79764befefeeeb142c80701291
PN Prathima <pn.prathima@citrix.com>
2020-08-16 01:41:58 +0800

    New Sopa project added

---
+++ deleteblob.ps1
@@ -0,0 +1,4 @@
+﻿$StorageAccountName = "casstoragearchivedevtest"
+$StorageAccountKey ="rmfw7x3W3em5zwzmE9WGTTFpBAp+OpBjuXQt8cW6JxyWZJNmamJrkcSooxHoaGE9qRx+ECDs+i9Fq0ZQi5WGQg=="
+$ctx = New-AzureStorageContext -StorageAccountName $StorageAccountName `
+         -StorageAccountKey $StorageAccountKey
